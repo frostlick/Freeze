3f6e8b05cc372a61b09a3b87ad148b248478e7fa
Bent Tranberg <bent.tranberg@gmail.com>
2021-03-09 00:06:17 +0100

    Using FileDialogsCmdMsg for initial demo

--- FreezeClient.Models/MainWindow.fs
+++ FreezeClient.Models/MainWindow.fs
@@ -1,1 +1,94 @@
 ﻿module FreezeClient.Models.MainWindow
+
+open System
+open Serilog
+open Serilog.Extensions.Logging
+open Elmish
+open Elmish.WPF
+
+type Model =
+    {
+        CurrentTime: DateTimeOffset
+        Text: string
+        StatusMsg: string
+    }
+
+type CmdMsg =
+    | Save of string
+    | Load
+
+let init () =
+    {
+        CurrentTime = DateTimeOffset.Now
+        Text = ""
+        StatusMsg = ""
+    }, []
+
+type Msg =
+    | SetTime of DateTimeOffset
+    | SetText of string
+    | RequestSave
+    | RequestLoad
+    | SaveSuccess
+    | LoadSuccess of string
+    | SaveCanceled
+    | LoadCanceled
+    | SaveFailed of exn
+    | LoadFailed of exn
+
+let update msg m =
+    match msg with
+    | SetTime t -> { m with CurrentTime = t }, []
+    | SetText s -> { m with Text = s}, []
+    | RequestSave -> m, [ Save m.Text ]
+    | RequestLoad -> m, [ Load ]
+    | SaveSuccess -> { m with StatusMsg = sprintf "Successfully saved at %O" DateTimeOffset.Now }, []
+    | LoadSuccess s -> { m with Text = s; StatusMsg = sprintf "Successfully loaded at %O" DateTimeOffset.Now }, []
+    | SaveCanceled -> { m with StatusMsg = "Saving canceled" }, []
+    | LoadCanceled -> { m with StatusMsg = "Loading canceled" }, []
+    | SaveFailed ex -> { m with StatusMsg = sprintf "Saving failed with exception %s: %s" (ex.GetType().Name) ex.Message }, []
+    | LoadFailed ex -> { m with StatusMsg = sprintf "Loading failed with exception %s: %s" (ex.GetType().Name) ex.Message }, []
+
+module Platform =
+
+    let bindings () : Binding<Model, Msg> list = [
+        "CurrentTime" |> Binding.oneWay (fun m -> m.CurrentTime)
+        "Text" |> Binding.twoWay ((fun m -> m.Text), SetText)
+        "StatusMsg" |> Binding.twoWay ((fun m -> m.StatusMsg), SetText)
+        "Save" |> Binding.cmd RequestSave
+        "Load" |> Binding.cmd RequestLoad
+        ]
+
+    let save text = async {
+        let dlg = Microsoft.Win32.SaveFileDialog ()
+        dlg.Filter <- "Text file (*.txt)|*.txt|Markdown file (*.md)|*.md"
+        let result = dlg.ShowDialog ()
+        if result.HasValue && result.Value then
+            do! IO.File.WriteAllTextAsync(dlg.FileName, text) |> Async.AwaitTask
+            return SaveSuccess
+        else
+            return SaveCanceled
+        }
+
+    let load () = async {
+        let dlg = Microsoft.Win32.OpenFileDialog ()
+        dlg.Filter <- "Text file (*.txt)|*.txt|Markdown file (*.md)|*.md"
+        dlg.DefaultExt <- "txt"
+        let result = dlg.ShowDialog ()
+        if result.HasValue && result.Value then
+            let! contents = IO.File.ReadAllTextAsync(dlg.FileName) |> Async.AwaitTask
+            return LoadSuccess contents
+        else
+            return LoadCanceled
+        }
+
+    let timerTick dispatch =
+        let timer = new Timers.Timer(1000.)
+        timer.Elapsed.Add (fun _ -> dispatch (SetTime DateTimeOffset.Now))
+        timer.Start()
+
+    let toCmd = function
+        | Save text -> Cmd.OfAsync.either save text id SaveFailed
+        | Load -> Cmd.OfAsync.either load () id LoadFailed
+
+let designVm = ViewModel.designInstance (init () |> fst) (Platform.bindings ())
